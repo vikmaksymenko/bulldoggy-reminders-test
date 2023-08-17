7aa07e703aaffc3a7479de43be51b56c928311ac
Viktor Maksymenko <vikmaksimenko@hotmail.com>
2023-08-17 11:31:00 +0200

    Implemented login feature scenarios

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -1,6 +1,29 @@
 Feature: Login to app
 
-    Scenario: Login with valid credentials
+    Scenario: Successul login with valid credentials
         Given I am on the login page
         When I enter credentials for user "pythonista"
         Then I should be logged in as "pythonista"
+
+    Scenario Outline: Login with wrong password or not registered user
+        Given I am on the login page
+        When I enter username "<username>" and password "<password>"
+        Then I should see the error message "Invalid login! Please retry."
+        And I should not be logged in
+
+        Examples:
+            | username            | password       |
+            | not_registered_user | password       |
+            | pythonista          | wrong_password |
+
+    # The sistem don't have specific error messages for empty email or password, so we have to setup separate scenario
+    Scenario Outline: Login with empty email or password
+        Given I am on the login page
+        When I enter username "<username>" and password "<password>"
+        And I should not be logged in
+
+        Examples:
+            | username   | password |
+            |            | password |
+            | pythonista |          |
+
--- cypress/support/step_definitions/login.ts
+++ cypress/support/step_definitions/login.ts
@@ -1,5 +1,15 @@
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
 
+const login = (username: string, password: string) => {
+    if (username) {
+        cy.get("input[name='username']").type(username);
+    }
+    if (password) {
+        cy.get("input[name='password']").type(password);
+    }
+    cy.get("[data-testid='login-button']").click();
+};
+
 Given("I am on the login page", () => {
     cy.visit("/login");
 });
@@ -7,13 +17,22 @@ Given("I am on the login page", () => {
 When("I enter credentials for user {string}", (name: string) => {
     cy.fixture("users").then((users) => {
         const user = users[name];
-        cy.get("input[name='username']").type(user.username);
-        cy.get("input[name='password']").type(user.password);
+        login(user.username, user.password);
     });
-    cy.get("[data-testid='login-button']").click();
 });
+
+When("I enter username {string} and password {string}", login);
 
 Then("I should be logged in as {string}", (name: string) => {
     cy.url().should("include", "/reminders");
     cy.get("#reminders-message").should("have.text", `Reminders for ${name}`);
+});
+
+Then("I should see the error message {string}", (message: string) => {
+    cy.get(".invalid-login-warning p").should("have.text", message);
+});
+
+Then("I should not be logged in", () => {
+    cy.url().should("include", "/login");
+    cy.get("#reminders-message").should("not.exist");
 });
